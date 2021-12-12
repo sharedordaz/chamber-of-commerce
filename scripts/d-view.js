25f8fe14eb05abd65dc7679de17cfc3c0ece6b9b
sharedordaz <sharedordaz@live.com.mx>
2021-12-11 19:01:22 -0600

    finished directory site

---
+++ scripts/d-view.js
@@ -0,0 +1,22 @@
+let grid_view = document.getElementById("grid_view");
+let list_view = document.getElementById("list_view");
+
+//console.log(grid_view);
+let directory_grid = document.querySelector(".directory-grid")
+
+
+
+grid_view.addEventListener("click", () => {
+
+    directory_grid.style.gridTemplate = "1fr 1fr 1fr/ 1fr 1fr";
+    grid_view.style.backgroundColor = "var(--orange)"
+    list_view.style.backgroundColor = "var(--blue)"
+});
+
+list_view.addEventListener("click", () =>{
+
+    directory_grid.style.gridTemplate = "repeat(7, 1fr)/ 1fr";
+    grid_view.style.backgroundColor = "var(--blue)"
+    list_view.style.backgroundColor = "var(--orange)"
+
+});
--- scripts/directory.js
+++ scripts/directory.js
@@ -11,24 +11,25 @@ fetch(requestURL)
 
         let card = document.createElement('div');
 
-        let h3 = document.createElement('h3');       
+        let h2 = document.createElement('h2');       
         let c_info = document.createElement("p");
         let c_link = document.createElement("button");
         let image = document.createElement("img");
 
-        h3.textContent = companies[i].name;
+        h2.textContent = companies[i].name;
         c_info.textContent = companies[i].contact_info;
+
         c_link.textContent = "Website";
-        c_link.setAttribute("href",companies[i].link); 
+        c_link.setAttribute("onclick", "window.open('" + companies[i].link + "', '_blank')"); 
 
 
         image.setAttribute('src', companies[i].logo); // pasted content
         
-        card.appendChild(h3);
+        card.appendChild(h2);
         card.appendChild(c_info);
         card.appendChild(c_link);
         card.appendChild(image)
-        document.querySelector('main').appendChild(card);
+        document.querySelector('.directory-grid').appendChild(card);
     }
   });
 
