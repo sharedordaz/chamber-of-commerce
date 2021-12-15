23f1e9000df7ec33ab56a02d2ab7b8d86a69a4fa
sharedordaz <sharedordaz@live.com.mx>
2021-12-14 19:13:12 -0600

    done a few changes on nav bar

--- scripts/d-view.js
+++ scripts/d-view.js
@@ -8,7 +8,7 @@ let directory_grid = document.querySelector(".directory-grid")
 
 grid_view.addEventListener("click", () => {
 
-    directory_grid.style.gridTemplate = "1fr 1fr 1fr/ 1fr 1fr";
+    directory_grid.style.gridTemplate = "1fr 1fr 1fr 1fr/ 1fr 1fr";
     grid_view.style.backgroundColor = "var(--orange)"
     list_view.style.backgroundColor = "var(--blue)"
 });
