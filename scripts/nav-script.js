e74092906859629b0a0fe30a420f611f74f4fa57
sharedordaz <sharedordaz@live.com.mx>
2021-12-15 20:53:39 -0600

    requirements

--- scripts/nav-script.js
+++ scripts/nav-script.js
@@ -2,7 +2,7 @@ let menu_icon = document.querySelector(".menu");
 let nav = document.querySelector("nav");
 
 let letters = document.querySelector(".absolute-grid");
-console.log(letters)
+//console.log(letters)
 //console.log(menu);
 menu_icon.addEventListener("click", show_menu)
 
